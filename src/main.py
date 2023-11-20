98acf72267e48665b6f3d4d9d96294d9ac98dca9
GTL <bess2010@inbox.ru>
2023-11-20 16:26:39 +0300

    add main button

--- src/main.py
+++ src/main.py
@@ -14,7 +14,7 @@ def send_welcome(message):
 def echo_all(message):
     # bot.send_message(message.chat.id, message.text)
     markup = types.ReplyKeyboardMarkup(row_width=3)
-    itembtn1 = types.WebAppInfo('https://194.135.22.212:5000/')
+    itembtn1 = types.WebAppInfo('https://sergeysablin99.github.io/ultimatecalendar/')
     bnt1 = types.KeyboardButton("mysite", web_app=itembtn1)
     markup.add(bnt1)
     bot.send_message(message.chat.id, "Choose one letter:", reply_markup=markup)
