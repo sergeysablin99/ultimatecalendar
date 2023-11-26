ec35fe72c73945c8e24a6d6f08576e508708475e
GTL <bess2010@inbox.ru>
2023-11-26 23:47:45 +0300

    now really on view

---
+++ vue/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <CarouselCalendar msg="Welcome to Your Vue.js App"/>
+</template>
+
+<script>
+import CarouselCalendar from './components/CarouserlCalendar/CarouselCalendar.vue'
+
+export default {
+  name: 'App',
+  components: {
+    CarouselCalendar
+  }
+}
+</script>
+
