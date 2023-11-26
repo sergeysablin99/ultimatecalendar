1550cd8f3da5f7c45ad58fcd5dea37f393e2d804
GTL <bess2010@inbox.ru>
2023-11-26 23:30:07 +0300

    now on vue

---
+++ docs/docs/src/components/CarouserlCalendar/utils/CalendarDateItemContainer.vue
@@ -0,0 +1,28 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="calendar-date-item-container">
+    <div class="calendar-date-item-day-of-week">
+      <slot name="day-of-week"></slot>
+    </div>
+    <div class="calendar-date-item-date">
+      <slot name="date"></slot>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+.calendar-date-item-date {
+  text-align: center;
+}
+
+.calendar-date-item-container {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  width: 20px;
+}
+</style>
---
+++ docs/docs/src/components/CarouserlCalendar/utils/CalendarHeader.vue
@@ -0,0 +1,49 @@
+<script setup>
+
+</script>
+
+<template>
+  <div id="calendar-header">
+    <div id="calendar-month-name-col">Ноябрь</div>
+    <div id="calendar-controls-col">
+      <div id="calendar-controls">
+        <button id="calendar-prev-button" class="calendar-control-buttons">&lt;</button>
+        <button id="calendar-next-button" class="calendar-control-buttons">&gt;</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+#calendar-header {
+  display: flex;
+  flex-direction: row;
+  width: 100%;
+  height: max-content;
+  justify-content: space-between;
+  align-items: center;
+  margin: 0 5px;
+}
+
+#calendar-controls {
+  display: flex;
+  flex-direction: row;
+  width: 100%;
+  height: max-content;
+  justify-content: space-between;
+}
+
+.calendar-control-buttons {
+  margin: 5px 5px;
+  border: 1px solid black;
+  padding: 1px 4px;
+  background-color: transparent;
+  text-align: center;
+  /* transition:*/
+  /*width 2s,*/
+  /*height 2s,*/
+  /*background-color 2s,*/
+  /*rotate 2s;*/
+}
+</style>
