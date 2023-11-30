53a71a2ab4047a40d4860f018c557e18b1dd7eaa
GTL <bess2010@inbox.ru>
2023-11-30 15:23:58 +0300

    dark theme

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -34,13 +34,13 @@ webApp.onEvent("themeChanged", () => alert("theme changed"))
 <style>
 
 :root {
-  --bg-color: #232e39; /* var(--tg-theme-bg-color, #232e39); */
-  --text-color: #ffffff; /* var(--tg-theme-text-color, #ffffff); */
-  --hint-color: #7f8c97; /* var(--tg-theme-hint-color, #7f8c97); */
-  --link-color: #6ab0d7; /* var(--tg-theme-link-color, #6ab0d7); */
-  --button-color: #60b0e1; /* var(--tg-theme-button-color, #60b0e1); */
-  --button-text-color: #ffffff; /* var(--tg-theme-button-text-color, #ffffff); */
-  --secondary-bg-color: #161f25; /* var(--tg-theme-secondary-bg-color, #161f25); */
+  --bg-color: var(--tg-theme-bg-color, #232e39);
+  --text-color: var(--tg-theme-text-color, #ffffff);
+  --hint-color: var(--tg-theme-hint-color, #7f8c97);
+  --link-color: var(--tg-theme-link-color, #6ab0d7);
+  --button-color: var(--tg-theme-button-color, #60b0e1);
+  --button-text-color: var(--tg-theme-button-text-color, #ffffff);
+  --secondary-bg-color: var(--tg-theme-secondary-bg-color, #161f25);
 
   --heading-font-size: 20px;
   --font-family: 'Roboto', sans-serif;
